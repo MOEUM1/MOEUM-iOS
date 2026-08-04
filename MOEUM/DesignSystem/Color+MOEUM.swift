c389e17b4349362bbbe2bf927f95d632a36f1c4d
Book-on <s40409841@gmail.com>
2026-08-04 19:28:07 +0900

    에셋및 부가 추가사항 첨가

---
+++ MOEUM/DesignSystem/Color+MOEUM.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+extension Color {
+    static let moeumGray50 = Color("Gray50")
+    static let moeumGray100 = Color("Gray100")
+    static let moeumGray200 = Color("Gray200")
+    static let moeumGray300 = Color("Gray300")
+    static let moeumGray400 = Color("Gray400")
+    static let moeumGray500 = Color("Gray500")
+    static let moeumGray600 = Color("Gray600")
+    static let moeumGray700 = Color("Gray700")
+    static let moeumGray800 = Color("Gray800")
+    static let moeumGray900 = Color("Gray900")
+
+    static let moeumMain50 = Color("Main50")
+    static let moeumMain100 = Color("Main100")
+    static let moeumMain200 = Color("Main200")
+    static let moeumMain300 = Color("Main300")
+    static let moeumMain400 = Color("Main400")
+    static let moeumMain500 = Color("Main500")
+    static let moeumMain600 = Color("Main600")
+    static let moeumMain700 = Color("Main700")
+    static let moeumMain800 = Color("Main800")
+    static let moeumMain900 = Color("Main900")
+
+    static let moeumCharacterLightBlue = Color("CharacterLightBlue")
+    static let moeumCharacterLightRed = Color("CharacterLightRed")
+    static let moeumCharacterLightYellow = Color("CharacterLightYellow")
+    static let moeumCharacterDarkYellow = Color("CharacterDarkYellow")
+    static let moeumCharacterLightPink = Color("CharacterLightPink")
+}
